90db07f29017993a17422be53fbf17949e8af54a
yizhiyu <yizhiyu_web@163.com>
2020-06-15 15:07:19 +0800

    处理 get 请求

--- document/004 - nodejs 处理 HTTP 请求.md
+++ document/004 - nodejs 处理 HTTP 请求.md
@@ -1,2 +1,2 @@
 - GET 请求
-  > get 请求，即哭护短要向 server 端获取数据，如查询博客列表，通过 querystring 来传递数据，如 a.html?a=100&b=200
+  > get 请求，即哭护短要向 server 端获取数据，如查询博客列表，通过 querystring 来传递数据，如 a.html?a=100&b=200,浏览器直接访问，返送 get 请求
--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 const http = require('http');
+const queryString = require('querystring');
 
 // 创建一个服务
 const server = http.createServer((req, res) => {
+  // req.method 判断前端是用什么方法进行请求的
+  // console.log('method', req.method); // GET
+
+  // 获取 URL
+  const url = req.url;
+  // console.log('query', url.split('?')[1]); // a=100&b=200
+  req.query = queryString.parse(url.split('?')[1]); // { a: '100', b: '200' }
+  // console.log(req.query); // { a: '100', b: '200' }
+
   // 结束了 返回一个 hi nodejs 字符串给前端
   res.end('hi nodejs');
 });
